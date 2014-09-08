85108c216f0b187f9c238c08a27e34ff5b1c01aa
Jessica Graham <jgraham@amplify.com>
2014-09-08 12:28:30 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(name='recipez',
+      version='1.0',
+      description='',
+      author='Jessica Graham',
+      author_email='jjgraham4@gmail.com',
+      url='',
+      packages=find_packages(),
+      install_requires=[
+        "Flask>=0.10.1"
+      ]
+)

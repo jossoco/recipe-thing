aefded9e65c1b19ced827743a6ed77138c410676
Jessica Graham <jgraham@amplify.com>
2014-09-10 16:19:19 -0400

    Find widest image on page and display it

--- src/recipe/parser.py
+++ src/recipe/parser.py
@@ -9,6 +9,7 @@ class RecipeParser():
         data['title'], data['sourceName'] = self._parse_title_and_source(soup)
         data['steps'] = self._parse_text_list(soup, "instruction")
         data['ingredients'] = self._parse_text_list(soup, "ingredient")
+        data['imageUrl'] = self._parse_image(soup)
         return data
 
     def _parse_title_and_source(self, soup):
@@ -31,6 +32,25 @@ class RecipeParser():
             if text.strip() != '':
                 list.append(text)
         return list
+
+    def _parse_image(self, soup):
+        images = soup.find_all('img')
+        image_src = ''
+        if len(images) > 1:
+            target_image = ''
+            max_width = 0
+            for image in images:
+                if image.attrs and 'width' in image.attrs:
+                    width = int(image.attrs['width'].replace('px', '').replace(';', ''))
+                    if width > max_width:
+                        max_width = width
+                        target_image = image
+            if target_image and 'src' in target_image.attrs:
+                print target_image
+                image_src = target_image.attrs['src']
+        elif len(images) == 1 and 'src' in images[0].attrs:
+            image_src = images[0].attrs['src']
+        return image_src
 
 
 def parse_recipe(input):

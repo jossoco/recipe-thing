3acbf3f3f6328d3422becedaca743b13ae0fa9fa
Jessica Graham <jgraham@amplify.com>
2014-09-09 22:19:19 -0400

    Move parsing to its own class

---
+++ src/recipe/parser.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import re
+
+class RecipeParser():
+
+    def parse_data(self, input):
+        soup = BeautifulSoup(input)
+        data = {}
+
+        title_parts = self._parseTitle(soup.title.string)
+        if type(title_parts) is tuple:
+            data['title'], data['sourceName'] = _parseTitle(soup.title.string)
+        else:
+            data['title'] = title_parts
+            data['sourceName'] = ''
+
+        data['steps'] = self._parseTextList(soup, "instruction")
+        data['ingredients'] = self._parseTextList(soup, "ingredient")
+        return data
+
+    def _parseTitle(self, title):
+        parts = title.split(' - ')
+        if len(parts) == 2:
+            return (parts[0], parts[1])
+        return title
+
+    def _parseTextList(self, soup, class_name):
+        list = []
+        els = soup.find_all(class_=class_name)
+        for el in els:
+            list.append(el.get_text())
+        return list
+
+
+def parse_recipe(input):
+    parser = RecipeParser()
+    return parser.parse_data(input)
--- src/recipez.py
+++ src/recipez.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from bs4 import BeautifulSoup
+from recipe.parser import parse_recipe
 import urllib2
 import json
 
@@ -9,41 +9,15 @@ app = Flask(__name__)
 def main():
     url = request.args.get('url', None)
     data = None
+
     if url:
         req = urllib2.Request(url)
         req.add_header('User-Agent', 'Recipe/1.0 +https://github.com/jossoco/recipe-thing')
         res = res.open = urllib2.urlopen(req)
-        data = _parse(res.read())
+        data = parse_recipe(res.read())
         data['url'] = url
+
     return render_template("index.html", data=json.dumps(data))
-
-def _parse(content):
-    soup = BeautifulSoup(content)
-    data = {}
-
-    title_parts = _parseTitle(soup.title.string)
-    if type(title_parts) is tuple:
-      data['title'], data['sourceName'] = _parseTitle(soup.title.string)
-    else:
-      data['title'] = title_parts
-      data['sourceName'] = ''
-
-    data['steps'] = _parseTextList(soup, "instruction")
-    data['ingredients'] = _parseTextList(soup, "ingredient")
-    return data
-
-def _parseTitle(title):
-    parts = title.split(' - ')
-    if len(parts) == 2:
-      return (parts[0], parts[1])
-    return title
-
-def _parseTextList(soup, class_name):
-    list = []
-    els = soup.find_all(class_=class_name)
-    for el in els:
-        list.append(el.get_text())
-    return list
 
 
 if __name__ == "__main__":

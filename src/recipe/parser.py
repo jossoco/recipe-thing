fb05f9a66b77cf6a435cd0ddf35f92a5aaebfde5
Jessica Graham <jgraham@amplify.com>
2014-09-10 13:32:30 -0400

    WIP try to parse unformatted recipes

--- src/recipe/parser.py
+++ src/recipe/parser.py
@@ -7,8 +7,8 @@ class RecipeParser():
         soup = BeautifulSoup(input)
         data = {}
         data['title'], data['sourceName'] = self._parse_title_and_source(soup)
-        data['steps'] = self._parse_text_list(soup, "instruction")
-        data['ingredients'] = self._parse_text_list(soup, "ingredient")
+        data['steps'] = self._parse_text_list(soup, 'instruction', ['recipeInstructions', 'instructions'])
+        data['ingredients'] = self._parse_text_list(soup, 'ingredient', ['ingredient', 'ingredients'])
         return data
 
     def _parse_title_and_source(self, soup):
@@ -23,9 +23,28 @@ class RecipeParser():
         else:
             return (title, '')
 
-    def _parse_text_list(self, soup, class_name):
+    def _parse_text_list(self, soup, class_name, item_props):
         list = []
         els = soup.find_all(class_=class_name)
+
+        if len(els) == 0:
+            child_els = []
+            for item_prop in item_props:
+                els = soup.find_all(attrs={'itemprop': item_prop})
+                if len(els) > 0:
+                    if len(els) == 1:
+                        child_type = None
+                        for c in els[0].contents:
+                            if c.name:
+                                if child_type is None:
+                                    child_type = c.name
+                                    child_els.append(c)
+                                elif c.name == child_type:
+                                    child_els.append(c)
+                    break
+            if len(child_els) > 0:
+                els = child_els
+
         for el in els:
             text = el.get_text()
             if text.strip() != '':
